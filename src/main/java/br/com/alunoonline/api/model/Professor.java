3fdea46c5be4f71d6f6aa4a0b30c24f675f12f75
Júlia <juliamenzss@gmail.com>
2025-03-25 10:29:00 -0300

    refactor: refatora importações de model de professor

--- src/main/java/br/com/alunoonline/api/model/Professor.java
+++ src/main/java/br/com/alunoonline/api/model/Professor.java
@@ -1,13 +1,7 @@
 package br.com.alunoonline.api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 
 @NoArgsConstructor
