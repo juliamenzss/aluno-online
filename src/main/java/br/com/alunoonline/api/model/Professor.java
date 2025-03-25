adb9dd492ffb11ab4b74cf7542cab1031ef92580
Júlia <juliamenzss@gmail.com>
2025-03-25 09:57:03 -0300

    feat: adiciona model de professor

---
+++ src/main/java/br/com/alunoonline/api/model/Professor.java
@@ -0,0 +1,26 @@
+package br.com.alunoonline.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "professor")
+@Entity
+public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+}
