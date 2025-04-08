969301a6413a214b973e5e363883e813a991bc98
Júlia <juliamenzss@gmail.com>
2025-04-08 14:20:50 -0300

    feat: adiciona model de disciplina

---
+++ src/main/java/br/com/alunoonline/api/model/Disciplina.java
@@ -0,0 +1,25 @@
+package br.com.alunoonline.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Disciplina implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @ManyToOne //vai ter chave estrangeira professor_id apontando p professor
+    @JoinColumn(name = "professor_id") //muitas disciplinas podem ter o mesmo professor
+    private Professor professor;
+}
